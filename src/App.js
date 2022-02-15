fe6e8f2396c7daa63aeb1bd20778d47011008821
yusefabucar <92749899+yusefabucar@users.noreply.github.com>
2022-02-15 09:53:34 -0500

    chat bot working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import "./App.css";
+import ChatBot from "react-simple-chatbot";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// ALL POSSIBLE THEME PROPS! //
+
+const theme = {
+  // background: "black",
+  //   fontFamily:
+  headerBgColor: "gray",
+  headerFontColor: "#fff",
+  // headerFontSize: '15px',
+  botBubbleColor: "green",
+  botFontColor: "#fff",
+  userBubbleColor: "blue",
+  userFontColor: "white",
+};
+
+const steps = [
+  {
+    id: "1",
+    message:
+      "What is your name? You can type it, or simply just say it! (click the mic icon below)",
+    trigger: "2",
+  },
+  {
+    id: "2",
+    user: true,
+    trigger: 3,
+  },
+  {
+    id: "3",
+    message:
+      "Hi {previousValue}, nice to meet you... What is one of your hobbies?",
+    trigger: 4,
+  },
+  {
+    id: "4",
+    user: true,
+    trigger: "5",
+  },
+  {
+    id: "5",
+    message: "Oh, you like {previousValue} too? Awesome!",
+  },
+];
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <ChatBot
+      headerTitle="ChatterBot"
+      recognitionEnable={true}
+      speechSynthesis={{ enable: true, lang: "en" }}
+      steps={steps}
+    />
+    ;
+  </ThemeProvider>
+);
 
 export default App;
